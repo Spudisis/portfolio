73250a9e14101bb029fded6a1d57bc592dcda941
Spudisis <alekasisbb@gmail.com>
2023-07-06 03:53:23 +0300

    coments

--- src/modules/info-module/components/slide-image/slide-image.tsx
+++ src/modules/info-module/components/slide-image/slide-image.tsx
@@ -2,41 +2,41 @@ import s from "./slide-image.module.scss";
 
 import mobxIcon from "../../pictures/mobx-icon-1024x1024-c9qpjcmj.png";
 import mobxIconWebp from "../../pictures/mobx-icon-1024x1024-c9qpjcmj.webp";
-import mobxIconAvif from "../../pictures/mobx-icon-1024x1024-c9qpjcmj.avif";
+// import mobxIconAvif from "../../pictures/mobx-icon-1024x1024-c9qpjcmj.avif";
 
 import reactIcon from "../../pictures/React-icon.svg.png";
 import reactIconWebp from "../../pictures/React-icon.svg.webp";
-import reactIconAvif from "../../pictures/React-icon.svg.avif";
+// import reactIconAvif from "../../pictures/React-icon.svg.avif";
 
 import reduxIcon from "../../pictures/redux-icon.png";
 import reduxIconWebp from "../../pictures/redux-icon.webp";
-import reduxIconAvif from "../../pictures/redux-icon.avif";
+// import reduxIconAvif from "../../pictures/redux-icon.avif";
 
 import js from "../../pictures/js.png";
 import jsWebp from "../../pictures/js.webp";
-import jsAvif from "../../pictures/js.avif";
+// import jsAvif from "../../pictures/js.avif";
 
 import ts from "../../pictures/ts.png";
 import tsWebp from "../../pictures/ts.webp";
-import tsAvif from "../../pictures/ts.avif";
+// import tsAvif from "../../pictures/ts.avif";
 
 import styledComponent from "../../pictures/styled-components.png";
 import styledComponentWebp from "../../pictures/styled-components.webp";
-import styledComponentAvif from "../../pictures/styled-components.avif";
+// import styledComponentAvif from "../../pictures/styled-components.avif";
 
 import webpack from "../../pictures/webpack.svg";
 
 import rhf from "../../pictures/rhf.png";
 import rhfWebp from "../../pictures/rhf.webp";
-import rhfAvif from "../../pictures/rhf.avif";
+// import rhfAvif from "../../pictures/rhf.avif";
 
 import sass from "../../pictures/sass.png";
 import sassWebp from "../../pictures/sass.webp";
-import sassAvif from "../../pictures/sass.avif";
+// import sassAvif from "../../pictures/sass.avif";
 
 import mui from "../../pictures/mui.png";
 import muiWebp from "../../pictures/mui.webp";
-import muiAvif from "../../pictures/mui.avif";
+// import muiAvif from "../../pictures/mui.avif";
 
 export const SlideImage = () => {
 	return (
