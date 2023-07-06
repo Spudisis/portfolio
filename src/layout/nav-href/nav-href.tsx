324cf3baa79b2edab8beeff3f1a68b829a9b3526
Spudisis <alekasisbb@gmail.com>
2023-07-06 03:21:57 +0300

    add nav images

--- src/layout/nav-href/nav-href.tsx
+++ src/layout/nav-href/nav-href.tsx
@@ -1,23 +1,41 @@
 import s from "./nav-href.module.scss";
 import gitHub from "./pictures/git.png";
-import tg from "./pictures/tg.webp";
+import gitHubWebp from "./pictures/git.webp";
+import gitHubAvif from "./pictures/git.avif";
+import tg from "./pictures/tg.png";
+import tgWebp from "./pictures/tg.webp";
+import tgAvif from "./pictures/tg.avif";
 import email from "./pictures/gmail.png";
+import emailWebp from "./pictures/gmail.webp";
+import emailAvif from "./pictures/gmail.avif";
 export const NavHref = () => {
 	return (
 		<nav className={s.navBar}>
 			<ul className={s.list}>
 				<li className={s.item_link}>
 					<a href="https://github.com/Spudisis" className={s.link} target="_blank">
-						<img src={gitHub} alt="icon github" className={s.image} />
+						<picture>
+							<source srcSet={gitHubAvif} type="image/avif" />
+							<source srcSet={gitHubWebp} type="image/webp" />
+							<img src={gitHub} alt="icon github" className={s.image} />
+						</picture>
 					</a>
 				</li>
 				<li className={s.item_link}>
 					<a href="https://t.me/Amevora" className={s.link} target="_blank">
-						<img src={tg} alt="icon tg" className={s.image} />
+						<picture>
+							<source srcSet={tgAvif} type="image/avif" />
+							<source srcSet={tgWebp} type="image/webp" />
+							<img src={tg} alt="icon tg" className={s.image} />
+						</picture>
 					</a>
 				</li>
 				<li className={s.item_link}>
-					<img src={email} alt="icon gmail" className={s.image} />
+					<picture>
+						<source srcSet={emailAvif} type="image/avif" />
+						<source srcSet={emailWebp} type="image/webp" />
+						<img src={email} alt="icon gmail" className={s.image} />
+					</picture>
 					<div className={s.email}>alekasisbb@gmail.com</div>
 				</li>
 			</ul>
